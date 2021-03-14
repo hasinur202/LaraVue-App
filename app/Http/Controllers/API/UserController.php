6594a5f1c08826411976fa9c92056bef57a6da3a
Hasinur Rahman <hasinur202@gmail.com>
2021-03-15 00:12:42 +0600

    fetch auth data in profile component

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -103,4 +103,9 @@ class UserController extends Controller
 
         return ['message' => 'User Deleted'];
     }
+
+    public function profile()
+    {
+        return auth('api')->user();
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -20,6 +20,6 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 
 Route::apiResources(['user' => 'API\UserController']);
 
-// Route::get('profile', 'API\UserController@profile');
+Route::get('profile', 'API\UserController@profile');
 // Route::get('findUser', 'API\UserController@search');
 // Route::put('profile', 'API\UserController@updateProfile');
