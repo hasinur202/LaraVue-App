e9a6d1d640d6a7060dc388e70d6053589c72c01c
Hasinur Rahman <hasinur202@gmail.com>
2021-03-21 22:28:07 +0600

    vue router 404 page

--- public/js/app.js
+++ public/js/app.js
@@ -83388,6 +83388,9 @@ var routes = [{
 }, {
   path: '/profile',
   component: __webpack_require__(/*! ./components/Profile.vue */ "./resources/js/components/Profile.vue")["default"]
+}, {
+  path: '*',
+  component: __webpack_require__(/*! ./components/NotFound.vue */ "./resources/js/components/NotFound.vue")["default"]
 }];
 var router = new vue_router__WEBPACK_IMPORTED_MODULE_4__["default"]({
   mode: 'history',
--- resources/js/app.js
+++ resources/js/app.js
@@ -72,7 +72,8 @@ const routes = [
     { path: '/dashboard', component: require('./components/Dashboard.vue').default },
     { path: '/users', component: require('./components/Users.vue').default },
     { path: '/developer', component: require('./components/Developer.vue').default },
-    { path: '/profile', component: require('./components/Profile.vue').default }
+    { path: '/profile', component: require('./components/Profile.vue').default },
+    { path: '*', component: require('./components/NotFound.vue').default }
   ]
 
   
